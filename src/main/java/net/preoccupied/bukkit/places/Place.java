db08c21a5ebe1d946c99ba99bb198bb2cdc5fb06
Christopher O'Brien <obriencj@gmail.com>
2011-08-23 18:40:56 -0400

    import

---
+++ src/main/java/net/preoccupied/bukkit/places/Place.java
@@ -0,0 +1,324 @@
+package net.preoccupied.bukkit.places;
+
+
+import com.avaje.ebean.validation.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import net.preoccupied.bukkit.permissions.PermissionCheck;
+
+
+
+@Entity()
+@Table(name="net_preoccupied_bukkit_place")
+public class Place {
+
+    @Id
+    private int id;
+
+    private String name = null;
+    private String title = null;
+
+    private boolean home = false;
+
+    private int weight = 0;
+
+    @Transient
+    private PermissionCheck permissionCheck = null;
+    
+    private String spot_w = null;
+    private int spot_x = 0;
+    private int spot_y = 0;
+    private int spot_z = 0;
+    private float spotYaw = 0.0f;
+
+    private String entrance_w = null;
+    private int entrance_x = 0;
+    private int entrance_y = 0;
+    private int entrance_z = 0;
+    private float entranceYaw = 0.0f;
+
+    private String graveyard_w = null;
+    private int graveyard_x = 0;
+    private int graveyard_y = 0;
+    private int graveyard_z = 0;
+    private float graveyardYaw = 0.0f;
+
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    
+    public String getName() {
+	return this.name;
+    }
+
+
+    public void setTitle(String title) {
+	this.title = title;
+    }
+
+
+    public String getTitle() {
+	return this.title;
+    }
+
+
+    public void setHome(boolean ishome) {
+	this.home = ishome;
+    }
+
+
+    public boolean isHome() {
+	return this.home;
+    }
+
+
+    public void setWeight(int weight) {
+	this.weight = weight;
+    }
+
+
+    public int getWeight() {
+	return this.weight;
+    }
+
+
+    public void setSpot(Location l) {
+	if(l != null) {
+	    setSpot_w(l.getWorld().getName());
+	    setSpot_x(l.getBlockX());
+	    setSpot_y(l.getBlockY());
+	    setSpot_z(l.getBlockZ());
+
+	} else {
+	    setSpot_w(null);
+	    setSpot_x(0);
+	    setSpot_y(0);
+	    setSpot_z(0);
+	}
+    }
+
+
+    public Location getSpot() {
+	Location l = asLocation(spot_w, spot_x, spot_y, spot_z);
+	l.setYaw(spotYaw);
+	return l;
+    }
+
+
+    public String getWorld() {
+	return getSpot_w();
+    }
+
+
+    public void setEntrance(Location l) {
+	if(l != null) {
+	    setEntrance_w(l.getWorld().getName());
+	    setEntrance_x(l.getBlockX());
+	    setEntrance_y(l.getBlockY());
+	    setEntrance_z(l.getBlockZ());
+	    setEntranceYaw(l.getYaw());
+
+	} else {
+	    setEntrance_w(null);
+	    setEntrance_x(0);
+	    setEntrance_y(0);
+	    setEntrance_z(0);
+	}
+    }
+
+
+    public Location getEntrance() {
+	Location l = asLocation(entrance_w, entrance_x, entrance_y, entrance_z);
+	l.setYaw(entranceYaw);
+	return l;
+    }
+
+
+    public void setGraveyard(Location l) {
+	if(l != null) {
+	    setGraveyard_w(l.getWorld().getName());
+	    setGraveyard_x(l.getBlockX());
+	    setGraveyard_y(l.getBlockY());
+	    setGraveyard_z(l.getBlockZ());
+	    setGraveyardYaw(l.getYaw());
+
+	} else {
+	    setGraveyard_w(null);
+	    setGraveyard_x(0);
+	    setGraveyard_y(0);
+	    setGraveyard_z(0);
+	}
+    }
+
+
+    public Location getGraveyard() {
+	Location l = asLocation(graveyard_w, graveyard_x, graveyard_y, graveyard_z);
+	l.setYaw(graveyardYaw);
+	return l;
+    }
+
+
+    public void setSpot_w(String w) {
+	this.spot_w = w;
+    }
+
+
+    public String getSpot_w() {
+	return spot_w;
+    }
+
+
+    public void setSpot_x(int i) {
+	this.spot_x = i;
+    }
+
+    
+    public int getSpot_x() {
+	return this.spot_x;
+    }
+
+
+    public void setSpot_y(int i) {
+	this.spot_y = i;
+    }
+
+    
+    public int getSpot_y() {
+	return this.spot_y;
+    }
+
+
+    public void setSpot_z(int i) {
+	this.spot_z = i;
+    }
+
+    
+    public int getSpot_z() {
+	return this.spot_z;
+    }
+
+
+    public void setEntrance_w(String w) {
+	this.entrance_w = w;
+    }
+
+
+    public String getEntrance_w() {
+	return entrance_w;
+    }
+
+
+    public void setEntrance_x(int i) {
+	this.entrance_x = i;
+    }
+
+    
+    public int getEntrance_x() {
+	return this.entrance_x;
+    }
+
+
+    public void setEntrance_y(int i) {
+	this.entrance_y = i;
+    }
+
+    
+    public int getEntrance_y() {
+	return this.entrance_y;
+    }
+
+
+    public void setEntrance_z(int i) {
+	this.entrance_z = i;
+    }
+
+    
+    public int getEntrance_z() {
+	return this.entrance_z;
+    }
+
+
+    public void setEntranceYaw(float yaw) {
+	this.entranceYaw = yaw;
+    }
+
+
+    public float getEntranceYaw() {
+	return this.entranceYaw;
+    }
+
+
+    public void setGraveyard_w(String w) {
+	this.graveyard_w = w;
+    }
+
+
+    public String getGraveyard_w() {
+	return graveyard_w;
+    }
+
+
+    public void setGraveyard_x(int i) {
+	this.graveyard_x = i;
+    }
+
+    
+    public int getGraveyard_x() {
+	return this.graveyard_x;
+    }
+
+
+    public void setGraveyard_y(int i) {
+	this.graveyard_y = i;
+    }
+
+    
+    public int getGraveyard_y() {
+	return this.graveyard_y;
+    }
+
+
+    public void setGraveyard_z(int i) {
+	this.graveyard_z = i;
+    }
+
+    
+    public int getGraveyard_z() {
+	return this.graveyard_z;
+    }
+
+
+    public void setGraveyardYaw(float yaw) {
+	this.graveyardYaw = yaw;
+    }
+
+
+    public float getGraveyardYaw() {
+	return this.graveyardYaw;
+    }
+
+
+    private static Location asLocation(String w, int x, int y, int z) {
+	if (w == null) return null;
+
+	World world = Bukkit.getServer().getWorld(w);
+	if (world == null) return null;
+
+	return new Location(world, (double) x + 0.5d, (double) y, (double) z + 0.5d);
+    }
+
+
+}
+
+
+/* The end. */
---
+++ src/main/java/net/preoccupied/bukkit/places/PlacesPlugin.java
@@ -0,0 +1,683 @@
+package net.preoccupied.bukkit.places;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.PersistenceException;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.preoccupied.bukkit.TeleportQueue;
+import net.preoccupied.bukkit.permissions.PermissionCommand;
+
+
+
+public class PlacesPlugin extends JavaPlugin {
+
+
+    public static final String SPAWN_NAME = "spawn";
+
+
+    private Map<String,Map<String,Place>> placesByName = null;
+    private Map<String,Map<String,Place>> homesByOwner = null;
+
+    private Map<String,Location> returnLocations = null;
+
+    private TeleportQueue teleportQueue = null;
+
+
+
+    public void onEnable() {
+	System.out.println("Places.onEnable()");
+
+	placesByName = new HashMap<String,Map<String,Place>>();
+	homesByOwner = new HashMap<String,Map<String,Place>>();
+
+	returnLocations = new HashMap<String,Location>();
+
+	teleportQueue = new TeleportQueue(this);
+	teleportQueue.enable();
+
+	loadPlaces();
+
+	setupCommands();
+    }
+
+
+
+    public void onDisable() {
+	placesByName.clear();
+	homesByOwner.clear();
+	returnLocations.clear();
+	teleportQueue.disable();
+    }
+
+
+
+    private void loadPlaces() {
+	try {
+	    for(Place p : getDatabase().find(Place.class).findList()) {
+		
+		Map<String,Map<String,Place>> storage = null;
+		Map<String,Place> ps = null;
+		
+		if(p.isHome()) {
+		    storage = homesByOwner;
+		} else {
+		    storage = placesByName;
+		}
+
+		String pworld = p.getWorld();
+		ps = storage.get(pworld);
+		if(ps == null) {
+		    ps = new HashMap<String,Place>();
+		    storage.put(pworld, ps);
+		}
+
+		ps.put(p.getName(), p);
+	    }
+	    
+	} catch(PersistenceException pe) {
+	    installDDL();
+	}
+    }
+
+
+
+    private Place getHome(Player player) {
+	return getHome(player.getLocation().getWorld().getName(), player.getName());
+    }
+
+
+
+    private Place getHome(String world, String owner) {
+	Map<String,Place> ps = homesByOwner.get(world);
+	if(ps == null) return null;
+	return ps.get(owner);
+    }
+
+
+
+    private Place getPlace(String world, String name) {
+	Map<String,Place> ps = placesByName.get(world);
+	if(ps == null) return null;
+	return ps.get(name);
+    }
+
+
+
+    private Place createHome(Player p) {
+	return createHome(p.getName(), p.getLocation());
+    }
+
+
+
+    private Place createHome(String owner, Location spot) {
+	return createPlace(owner, spot, true);
+    }
+
+
+
+    private Place createPlace(String name, Location spot) {
+	return createPlace(name, spot, false);
+    }
+
+
+
+    private Place createPlace(String n, Location l, boolean home) {
+	Place p = new Place();
+	p.setName(n);
+	p.setSpot(l);
+	p.setHome(home);
+
+	Map<String,Map<String,Place>> storage = null;
+	if(home) {
+	    storage = homesByOwner;
+	} else {
+	    storage = placesByName;
+	}
+
+	String pworld = l.getWorld().getName();
+	Map<String,Place> ps = storage.get(pworld);
+	if(ps == null) {
+	    ps = new HashMap<String,Place>();
+	    storage.put(pworld, ps);
+	}
+	ps.put(n, p);
+
+	getDatabase().save(p);
+	return p;
+    }
+
+
+    
+    private void deletePlace(Place p) {
+	Map<String,Map<String,Place>> storage = null;
+	if(p.isHome()) {
+	    storage = homesByOwner;
+	} else {
+	    storage = placesByName;
+	}
+
+	Map<String,Place> ps = storage.get(p.getWorld());
+	if(ps != null) ps.remove(p.getName());
+       
+	getDatabase().delete(p);
+    }
+
+
+
+    private void updatePlace(Place p) {
+	getDatabase().update(p);
+    }
+
+
+
+    public void setupCommands() {
+	new PermissionCommand(this, "home") {
+	    public boolean run(Player p) {
+		Place home = getHome(p);
+		if(home == null) {
+		    msg(p, "You've not set your home. You may do so via /set-home");
+
+		} else {
+		    saveReturn(p);
+		    teleportQueue.safeTeleport(p, home.getSpot());
+		}
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "set-home") {
+	    public boolean run(Player p) {
+		Place home = getHome(p);
+		if(home == null) {
+		    home = createHome(p);
+		} else {
+		    home.setSpot(p.getLocation());
+		    updatePlace(home);
+		}
+
+		msg(p, "Home location has been saved");
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "visit") {
+	    public boolean run(Player p, String n) {
+		Player friend = getServer().getPlayer(n);
+		if(friend == null) {
+		    msg(p, "Player not found: " + n);
+		    return true;
+		}
+
+		saveReturn(p);
+		teleportQueue.safeTeleport(p, friend.getLocation());
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "graveyard") {
+	    public boolean run(Player p) {
+		Nearness near = getNearest(p);
+		Place place = near.getPlace();
+
+		saveReturn(p);
+		teleportQueue.safeTeleport(p, place.getGraveyard());
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "return") {
+	    public boolean run(Player p) {
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "spawn") {
+	    public boolean run(Player p) {
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "set-spawn") {
+	    public boolean run(Player p, String n) {
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "add-place") {
+	    public boolean run(Player p, String n) {
+		return run(p, n, "0");
+	    }
+
+	    public boolean run(Player p, String n, String weight) {
+		Place place = getPlace(p.getWorld().getName(), n);
+		if(place != null) {
+		    msg(p, "A place with this name already exists: " + n);
+		    return true;
+		}
+		
+		int w = parseInt(p, weight, 0);
+
+		place = createPlace(n, p.getLocation());
+		place.setWeight(w);
+		updatePlace(place);
+
+		msg(p, "Created new place: " + n);
+		
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "remove-place") {
+	    public boolean run(Player p, String n) {
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "set-place-weight") {
+	    public boolean run(Player p, String n, String weight) {
+		Place place = getPlace(p.getWorld().getName(), n);
+		if(place == null) {
+		    msg(p, "No such place: " + n);
+		    return true;
+		}
+
+		int w = 0;
+		try {
+		    w = Integer.parseInt(weight);
+		} catch(Exception e) {
+		    msg(p, "Invalid number format: " + weight);
+		    return true;
+		}
+
+		place.setWeight(w);
+		updatePlace(place);
+		msg(p, "Place '" + n + "' weight set to: " + w);
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "set-place") {
+	    public boolean run(Player p, String n) {
+		Place place = getPlace(p.getWorld().getName(), n);
+		if(place == null) {
+		    msg(p, "No such place: " + n);
+		    return true;
+		}
+
+		place.setSpot(p.getLocation());
+		updatePlace(place);
+		msg(p, "Place '" + n + "' updated to current location.");
+		
+		return true;
+	    }
+
+	    public boolean run(Player p, String n, String weight) {
+		Place place = getPlace(p.getWorld().getName(), n);
+		if(place == null) {
+		    msg(p, "No such place: " + n);
+		    return true;
+		}
+
+		int w = 0;
+		try {
+		    w = Integer.parseInt(weight);		    
+		} catch(Exception e) {
+		    msg(p, "Invalid number format: " + weight);
+		    return true;
+		}
+
+		place.setSpot(p.getLocation());
+		place.setWeight(w);
+		updatePlace(place);
+		msg(p, "Place '" + n + "' updated to current location with weight " + w);
+
+		return true;
+	    }
+	};
+
+	
+	new PermissionCommand(this, "set-place-entrance") {
+	    public boolean run(Player p, String n) {
+		Place place = getPlace(p.getWorld().getName(), n);
+		if(place == null) {
+		    msg(p, "No such place: " + n);
+		    return true;
+		}
+
+		place.setEntrance(p.getLocation());
+		updatePlace(place);
+		msg(p, "Place '" + n + "' entrance updated to current location.");
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "set-place-graveyard") {
+	    public boolean run(Player p, String n) {
+		Place place = getPlace(p.getWorld().getName(), n);
+		if(place == null) {
+		    msg(p, "No such place: " + n);
+		    return true;
+		}
+
+		place.setGraveyard(p.getLocation());
+		updatePlace(place);
+		msg(p, "Place '" + n + "' graveyard updated to current location.");
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "whereis") {
+	    public boolean run(Player p, String n) {
+		Player friend = getServer().getPlayer(n);
+		if(friend == null) {
+		    msg(p, "Player not found: " + n);
+		    return true;
+		}
+
+		Nearness near = getNearest(friend.getLocation());
+		Direction dir = getDirection(p, near);
+		Place place = near.place;
+
+		msg(p, friend.getName() + " is " + dir.getDisplay() + " " + place.getName());
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "whereami") {
+	    public boolean run(Player p) {
+		Nearness near = getNearest(p);
+		Direction dir = getDirection(p, near);
+		Place place = near.getPlace();
+
+		msg(p, "You are " + dir.getDisplay() + " " + place.getName());
+
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "nearby") {
+	    public boolean run(Player p) {
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "jail") {
+	    public boolean run(Player p, String n) {
+		return true;
+	    }
+
+	    public boolean run(Player p, String n, String minutes) {
+		return true;
+	    }
+	};
+
+
+	new PermissionCommand(this, "unjail") {
+	    public boolean run(Player p, String n) {
+		return true;
+	    }
+	};
+
+    }
+
+
+
+    private int getDistance(int x1, int y1, int z1, int x2, int y2, int z2) {
+	int xd, yd, zd;
+	xd = x1 - x2;
+	yd = y1 - y2;
+	zd = z1 - z2;
+	return (int) Math.sqrt((xd * xd) + (yd * yd) + (zd * zd));
+    }
+
+
+
+    private int getDistance(Location a, Location b) {
+	int x1, y1, z1;
+	int x2, y2, z2;
+
+	x1 = a.getBlockX();
+	y1 = a.getBlockY();
+	z1 = a.getBlockZ();
+
+	x2 = b.getBlockX();
+	y2 = b.getBlockY();
+	z2 = b.getBlockZ();
+
+	return getDistance(x1, y1, z1, x2, y2, z2);	
+    }
+
+
+   
+    private int getWeightedDistance(Location a, Place b) {
+	return getDistance(a, b.getSpot()) - b.getWeight();
+    }
+
+
+
+    private int getFlatDistance(int x1, int z1, int x2, int z2) {
+	int xd, zd;
+	xd = x1 - x2;
+	zd = z1 - z2;
+	return (int) Math.sqrt((xd * xd) + (zd * zd));
+    }
+
+
+
+    private int getFlatDistance(Location a, Location b) {
+	return getFlatDistance(a.getBlockX(), a.getBlockZ(),
+			       b.getBlockX(), b.getBlockZ());
+    }
+
+
+
+    private int getWeightedFlatDistance(Location a, Place place) {
+	return getFlatDistance(place.getSpot(), a) - place.getWeight();
+    }
+
+
+
+    private Nearness getNearest(Player player) {
+	return getNearest(player.getLocation());
+    }
+
+
+
+    private Nearness getNearest(Location l) {
+	Place ret = null;
+	int distance = 0;
+
+	World w = l.getWorld();
+	Map<String,Place> places = placesByName.get(w.getName());
+	if(places == null) return null;
+
+	for(Place place : places.values()) {
+	    if(ret == null) {
+		ret = place;
+		distance = getWeightedDistance(l, place);
+
+	    } else {
+		int d = getWeightedDistance(l, place);
+		
+		// if place is closer than ret, or we're inside of
+		// place and place is smaller than ret, then place is
+		// our new closest.
+		if((d < distance) || ((d <= 0) && place.getWeight() < ret.getWeight())) {
+		    distance = d;
+		    ret = place;
+		}
+	    }
+	}
+	    
+	return (ret == null)? null: new Nearness(ret, distance);
+    }
+
+
+    
+    private static class Nearness implements Comparable<Nearness> {
+	Place place;
+	int distance;
+	Nearness(Place place, int distance) {
+	    this.place = place;
+	    this.distance = distance;
+	}
+	public int compareTo(Nearness c) {
+	    return distance - c.distance;
+	}
+	int getDistance() {
+	    return distance;
+	}
+	Place getPlace() {
+	    return place;
+	}
+    }
+
+
+
+    private static enum Direction {
+	IN ("in"),
+	OUT ("out"),
+	EAST ("east of"),
+	NORTHEAST ("northeast of"),
+	NORTH ("north of"),
+	NORTHWEST ("northwest of"),
+	WEST ("west of"),
+	SOUTHWEST ("southwest of"),
+	SOUTH ("south of"),
+	SOUTHEAST ("southeast of"),
+	ABOVE ("above"),
+	BELOW ("below");
+
+	String display;
+
+	Direction(String display) {
+	    this.display = display;
+	}
+
+	String getDisplay() {
+	    return display;
+	}
+
+	Direction getReverse() {
+	    switch(this) {
+	    case IN: return OUT;
+	    case OUT: return IN;
+	    case EAST: return WEST;
+	    case NORTHEAST: return SOUTHWEST;
+	    case NORTH: return SOUTH;
+	    case NORTHWEST: return SOUTHEAST;
+	    case WEST: return EAST;
+	    case SOUTHWEST: return NORTHEAST;
+	    case SOUTH: return NORTH;
+	    case SOUTHEAST: return NORTHWEST;
+	    case ABOVE: return BELOW;
+	    case BELOW: return ABOVE;
+	    default: return OUT;
+	    }
+	}
+    };
+    
+
+
+    private Direction getDirection(Player player, Nearness near) {
+	return getDirection(player.getLocation(), near);
+    }
+
+
+
+    private Direction getDirection(Location loc, Nearness near) {
+	if(near.getDistance() <= 0) {
+	    return Direction.IN;
+
+	} else {
+	    int wfd = getWeightedFlatDistance(loc, near.place);
+	    if(wfd <= 0) {
+		return getVerticalDirection(loc, near.place.getSpot());
+	    } else {
+		return getFlatDirection(loc, near.place.getSpot());
+	    }
+	}	
+    }
+
+
+
+    private Direction getVerticalDirection(Location a, Location b) {
+	int yd = a.getBlockY() - b.getBlockY();
+	return (yd > 0)? Direction.ABOVE: Direction.BELOW;
+    }
+
+
+
+    /* Direction is A relative to B, thus "A is NORTHWEST of B" */
+    private Direction getFlatDirection(Location a, Location b) {
+	int xd, zd;
+	xd = a.getBlockX() - b.getBlockX();
+	zd = a.getBlockZ() - b.getBlockZ();
+
+	double deg = Math.atan2(xd, zd);
+	if(deg < 22.5) {
+	    return Direction.EAST;
+	} else if(deg < 67.5) {
+	    return Direction.NORTHEAST;
+	} else if(deg < 112.5) {
+	    return Direction.NORTH;
+	} else if(deg < 157.5) {
+	    return Direction.NORTHWEST;
+	} else if(deg < 202.5) {
+	    return Direction.WEST;
+	} else if(deg < 247.5) {
+	    return Direction.SOUTHWEST;
+	} else if(deg < 292.5) {
+	    return Direction.SOUTH;
+	} else if(deg < 337.5) {
+	    return Direction.SOUTHEAST;
+	} else {
+	    // if(deg <= 360)
+	    return Direction.EAST;
+	}
+    }
+
+
+
+    public List<Class<?>> getDatabaseClasses() {
+	List<Class<?>> list = new ArrayList<Class<?>>();
+	list.add(Place.class);
+	return list;
+    }
+
+
+}
+
+
+
+/* The end. */
